69b3d459fb9f66a0e9c35578bce16314bef692d8
larrisald202 <larrisald202@gmail.com>
2026-03-15 00:37:15 -0300

    Update Brookhaven RP.lua

--- Brookhaven RP.lua
+++ Brookhaven RP.lua
@@ -1,4 +1,4 @@
-local Libary = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+local Libary = loadstring(game:HttpGet("https://raw.githubusercontent.com/enzoplaaygamemg12/RedzhubUiRedux/refs/heads/main/RedzHubUiLib.lua"))()
 workspace.FallenPartsDestroyHeight = -math.huge
 
 local Window = Libary:MakeWindow({
